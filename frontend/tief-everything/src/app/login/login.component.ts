a0d958d11b1784bcd0babb37ce635b1715f33ee2
Thembani Maswanganyi <t.maswanganyi101@gmail.com>
2023-08-08 11:49:26 +0200

    update

--- frontend/tief-everything/src/app/login/login.component.ts
+++ frontend/tief-everything/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,9 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  
   isLoginMode = true;
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form.value);
+    form.reset();
   }
 
 }
