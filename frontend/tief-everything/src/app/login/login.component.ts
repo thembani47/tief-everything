0b46a79556b96b110a2d5e107b0ee6d5b62ace77
Thembani Maswanganyi <t.maswanganyi101@gmail.com>
2023-08-08 11:04:02 +0200

    Switch modes
    switch between sign in and sign up...

--- frontend/tief-everything/src/app/login/login.component.ts
+++ frontend/tief-everything/src/app/login/login.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  isLoginMode = true;
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
 
 }
